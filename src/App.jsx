da413640b0e1c6a508ab7d75581d4800254bfb97
BisuMandi <mandib694@gmail.com>
2026-08-23 14:31:53 +0530

    implement dynamic 4x4 card grid rendering

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
+import { Card } from "./components/Card";
+import { GameHeader } from "./components/GameHeader";
+  
+const cardValues = ["🍎", "🍇", "🥭", "🍌", "🥝", "🍉", "🥑","🍒", "🍎", "🍇", "🥭", "🍌", "🥝", "🍉", "🥑","🍒"]
 
 const App = () => {
 
   return (
-    <>
-     <h1>Memory Card Game</h1>
-    </>
+    <div className="app">
+      <GameHeader score={2} moves={3} />
+
+      <div className="cards-grid">
+        {cardValues.map((card, index) =>
+          <Card card={card} />)}
+      </div>
+    </div>
   )
 }
 
-export default App
+export default App;
